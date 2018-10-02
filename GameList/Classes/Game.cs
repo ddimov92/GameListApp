46e4ed1c8e58dbaba10570bc21713a56e89e02a5
snooprs <33538246+snooprs@users.noreply.github.com>
2018-10-02 20:24:41 +0300

    Add project files.

---
+++ GameList/Classes/Game.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using System.Threading;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Media;
+using GameList.Classes;
+using Windows.UI;
+
+namespace GameList.Classes
+{
+    [DataContract(Name = "Gamez", Namespace = "TestNamespace")]
+    public class Game
+    {
+        [DataMember]
+        public int Number { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int Year { get; set; }
+        [DataMember]
+        public string Genre { get; set; }
+        [DataMember]
+        public string Platform { get; set; }
+        [DataMember]
+        public Uri Uri { get; set; }
+        [DataMember]
+        public bool Owned { get; set; }
+
+
+
+
+        public SolidColorBrush BackgroundColor { get; set; }
+        public BitmapImage Image { get; set; }
+
+    }
+
+    
+    public class GameManager 
+    {
+
+        public static void GetAllGames(ObservableCollection<Game> games)
+        {
+            var allGames = GetGames();
+            games.Clear();
+            allGames.ForEach(p => games.Add(p));
+            foreach (Game game in games)
+            {
+                game.Image = new BitmapImage(game.Uri);
+            }
+        }
+
+        public static List<Game> GetGames()
+        {
+
+            var gameList = new List<Game>
+            {
+                #region Original Collection
+               
+                new Game() { Number = 1, Name = "Dishonored 2", Year = 2016, Genre = "Action-adventure", Platform = "PC",
+                    Uri = new Uri("ms-appx:///Assets/Dishonored-2.jpg"), Image = new BitmapImage() },
+                new Game() { Number = 2, Name = "Watch Dogs 2", Year = 2016, Genre = "Action-adventure", Platform = "Playstation 4",
+                    Uri = new Uri("ms-appx:///Assets/Watch-Dogs-2.jpg") },
+                new Game() { Number = 3, Name = "Civilization VI", Year = 2016, Genre = "Strategy", Platform = "PC",
+                    Uri = new Uri("ms-appx:///Assets/Civlization-6.jpg") },
+                new Game() { Number = 4, Name = "Steep", Year = 2016, Genre = "Sports", Platform = "Xbox One",
+                    Uri = new Uri("ms-appx:///Assets/Steep.jpg") },
+                new Game() { Number = 5, Name = "Planet Coaster", Year = 2016, Genre = "Simulation", Platform = "PC",
+                    Uri = new Uri("ms-appx:///Assets/Planet-Coaster.jpg") },
+                new Game() { Number = 6, Name = "Mafia 3", Year = 2016, Genre = "Action-adventure", Platform = "Playstation 4",
+                    Uri = new Uri("ms-appx:///Assets/Mafia-3.png") },
+                new Game() { Number = 7, Name = "Rainbow Six Siege", Year = 2015, Genre = "FPS", Platform = "PC",
+                    Uri = new Uri("ms-appx:///Assets/Rainbow-Six-Siege.jpg") },
+                new Game() { Number = 8, Name = "Astroneer", Year = 2016, Genre = "Sandbox", Platform = "PC",
+                    Uri = new Uri("ms-appx:///Assets/Astroneer.png") },
+                new Game() { Number = 9, Name = "Rimworld", Year = 2013, Genre = "Simulation", Platform = "PC",
+                    Uri = new Uri("ms-appx:///Assets/Rimworld.jpg") },
+                new Game() { Number = 10, Name = "Warhammer Vermintide", Year = 2016, Genre = "FPS", Platform = "PC",
+                    Uri = new Uri("ms-appx:///Assets/Warhammer-Vermintide.jpg") },
+                new Game() { Number = 11, Name = "Just Cause 3", Year = 2015, Genre = "FPS", Platform = "Xbox One",
+                    Uri = new Uri("ms-appx:///Assets/Just-Cause-3.jpg") },
+                new Game() { Number = 12, Name = "Assassin's Creed Unity", Year = 2014, Genre = "Action-adventure", Platform = "Playstation 4",
+                    Uri = new Uri("ms-appx:///Assets/Assassin's-Creed-Unity.jpg") },
+                new Game() { Number = 13, Name = "Batman Arkham Origins", Year = 2015, Genre = "Action-adventure", Platform = "PC",
+                    Uri = new Uri("ms-appx:///Assets/Batman-Arkham-Origins.jpg") },
+                new Game() { Number = 14, Name = "Ghost Recon Wildlands", Year = 2017, Genre = "Action-adventure", Platform = "PC",
+                    Uri = new Uri("ms-appx:///Assets/Ghost-Recon-Wildlands.jpg") },
+                new Game() { Number = 15, Name = "Resident Evil 7", Year = 2017, Genre = "Horror", Platform = "PC",
+                    Uri = new Uri("ms-appx:///Assets/Resident-Evil-7.jpg") },
+                new Game() { Number = 16, Name = "Conan Exiles", Year = 2017, Genre = "Survival", Platform = "PC",
+                    Uri = new Uri("ms-appx:///Assets/Conan-Exiles.jpg") },
+                new Game() { Number = 17, Name = "For Honor", Year = 2017, Genre = "Action", Platform = "PC",
+                    Uri = new Uri("ms-appx:///Assets/For-Honor.jpg") }
+                    
+                #endregion
+                
+               
+            };
+            foreach (Game game in gameList)
+            {
+                if (game.Owned == true)
+                {
+                    SolidColorBrush colorBrush = new SolidColorBrush();
+                    game.BackgroundColor = new SolidColorBrush(colorBrush.Color = Colors.Green);
+                }
+                else
+                {
+                    SolidColorBrush colorBrush = new SolidColorBrush();
+                    game.BackgroundColor = new SolidColorBrush(colorBrush.Color = Colors.Red);
+                }
+            }
+            return gameList;
+        }
+    }
+}
---
+++ GameList/MainPage.xaml.cs
@@ -0,0 +1,300 @@
+﻿using GameList.Classes;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using System.Xml;
+using System.Runtime.Serialization.Json;
+using System.Xml.Serialization;
+using System.Threading.Tasks;
+using System.Text;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage;
+using Windows.UI.Xaml.Input;
+using Windows.System;
+
+
+
+
+
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace GameList
+{
+    public sealed partial class MainPage : Page
+    {
+
+        public ObservableCollection<Game> games;
+        public ObservableCollection<Game> filteredGames;
+        public List<BitmapImage> List;
+        public Brush myBrush = null;
+        public string uriPath;
+        public Game chosenGame;
+        public string storageFolder;
+        public int yearInt;
+       
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+
+
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            string path = System.IO.Path.Combine(folder.Path, @"XmlStorageFile.xml");
+            storageFolder = path;
+            
+
+
+            if (File.Exists(path))
+            {
+                Deserialize(storageFolder);
+            }
+            else
+            {
+                games = new ObservableCollection<Game>();
+                GameManager.GetAllGames(games);
+                Serialize(storageFolder);
+            }
+
+            #region Allow this part to see where the app has created its storage file
+            //PathTextBox.Visibility = Visibility.Visible;
+            //string pathWithoutFile = storageFolder.Substring(0, storageFolder.Length - 18);
+            //PathTextBox.Text = pathWithoutFile;
+            #endregion
+
+        }
+        private async void GridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var chosenGame = (Game)e.ClickedItem;
+            this.chosenGame = chosenGame;
+
+            if (this.chosenGame.Owned == true)
+            {
+                ContentDialog dialog = new ContentDialog()
+                {
+                    IsEnabled = true,
+                    Content = "What do you want to do",
+                    PrimaryButtonText = "I don't own this game",
+                    SecondaryButtonText = "Delete the game"
+                };
+                dialog.PrimaryButtonClick += Dialog_PrimaryButtonClick;
+                dialog.SecondaryButtonClick += Dialog_SecondaryButtonClick;
+                await dialog.ShowAsync();
+            }
+            else
+            {
+                ContentDialog dialog2 = new ContentDialog()
+                {
+                    IsEnabled = true,
+                    Content = "What do you want to do",
+                    PrimaryButtonText = "I own this game",
+                    SecondaryButtonText = "Delete the game"
+                };
+                dialog2.PrimaryButtonClick += Dialog2_PrimaryButtonClick;
+                dialog2.SecondaryButtonClick += Dialog2_SecondaryButtonClick;
+                await dialog2.ShowAsync();
+            }
+            
+        }
+        #region Content Dialogs
+        //1
+        private void Dialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            games.Remove(chosenGame);
+            Serialize(storageFolder);
+            MyFrame.Navigate(typeof(MainPage));
+        }
+
+        private void Dialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            chosenGame.Owned = false;
+            Serialize(storageFolder);
+            MyFrame.Navigate(typeof(MainPage));
+        }
+        //2
+        private void Dialog2_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            games.Remove(chosenGame);
+            Serialize(storageFolder);
+            MyFrame.Navigate(typeof(MainPage));
+        }
+
+        private void Dialog2_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            chosenGame.Owned = true;
+            Serialize(storageFolder);
+            MyFrame.Navigate(typeof(MainPage));
+        }
+        
+        #endregion
+
+
+        public void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (int.TryParse(YearTextBox.Text, out yearInt))
+            {
+                yearInt = Int32.Parse(YearTextBox.Text);
+            }
+            else
+            {
+                YearTextBox.Text = String.Empty;
+            }
+
+            
+            if (NameTextBox.Text == String.Empty || YearTextBox.Text == String.Empty || uriPath == String.Empty || PlatformTextBox.Text == String.Empty)
+            {
+                #region Placeholder Text Changed
+                if (NameTextBox.Text == String.Empty)
+                {
+                    NameTextBox.PlaceholderText = "Please enter a name";
+                    NameTextBox.Background = new SolidColorBrush(Colors.Red);
+                }
+                if (GenreTextBox.Text == String.Empty)
+                {
+                    GenreTextBox.PlaceholderText = "Please enter a genre";
+                    GenreTextBox.Background = new SolidColorBrush(Colors.Red);
+                }
+                if (PlatformTextBox.Text == String.Empty)
+                {
+                    PlatformTextBox.PlaceholderText = "Please enter a platform";
+                    PlatformTextBox.Background = new SolidColorBrush(Colors.Red);
+                }
+                if (uriPath == null)
+                {
+                    DropPanel.Background = new SolidColorBrush(Colors.Red);
+                }
+                #endregion
+
+                return;
+            }
+            else
+            {
+                string name = NameTextBox.Text;
+                
+                string genre = GenreTextBox.Text;
+                string platform = PlatformTextBox.Text;
+
+                Game addedGame = new Game()
+                {
+                    Number = games.Count + 1,
+                    Name = name,
+                    Year = yearInt,
+                    Genre = genre,
+                    Platform = platform,
+                    Uri = new Uri(uriPath)
+                };
+                games.Add(addedGame);
+                foreach (Game game in games)
+                {
+                    game.Image = new BitmapImage(game.Uri);
+                }
+                Serialize(storageFolder);
+                Deserialize(storageFolder);
+
+                NameTextBox.Text = String.Empty;
+                YearTextBox.Text = String.Empty;
+                GenreTextBox.Text = String.Empty;
+                PlatformTextBox.Text = String.Empty;
+                MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+            }
+        }
+
+        public void Serialize(string storageFolder)
+        {
+            FileStream writer = new FileStream(storageFolder, FileMode.Create, FileAccess.ReadWrite);
+            DataContractSerializer serializer = new DataContractSerializer(typeof(ObservableCollection<Game>));
+            serializer.WriteObject(writer, games);
+            writer.Dispose();
+        }
+
+        public void Deserialize(string storageFolder)
+        {
+            DataContractSerializer serializer = new DataContractSerializer(typeof(ObservableCollection<Game>));
+            FileStream filestream = new FileStream(storageFolder, FileMode.Open, FileAccess.ReadWrite);
+            XmlDictionaryReader reader = XmlDictionaryReader.CreateTextReader(filestream, new XmlDictionaryReaderQuotas());
+            games = (ObservableCollection<Game>)serializer.ReadObject(reader);
+            
+            foreach (Game game in games)
+            {
+                game.Image = new BitmapImage(game.Uri);
+
+                if (game.Owned == true)
+                {
+                    SolidColorBrush colorBrush = new SolidColorBrush();
+                    game.BackgroundColor = new SolidColorBrush(colorBrush.Color = Colors.Green);
+                }
+                else
+                {
+                    SolidColorBrush colorBrush = new SolidColorBrush();
+                    game.BackgroundColor = new SolidColorBrush(colorBrush.Color = Colors.Red);
+                }
+            }
+            filestream.Dispose();
+        }
+
+        private async void DropPanel_Drop(object sender, DragEventArgs e)
+        {
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var items = await e.DataView.GetStorageItemsAsync();
+
+                if (items.Any())
+                {
+                    var storageFile = (StorageFile)items[0];
+                    var contentType = storageFile.ContentType;
+
+                    StorageFolder folder = ApplicationData.Current.LocalFolder;
+                    
+
+                    if (contentType == "image/png" || contentType == "image/jpeg" || contentType == "image/gif" ||
+                        contentType == "image/pjpeg" || contentType == "image/bmp")
+                    {
+                        StorageFile newFile = await storageFile.CopyAsync(folder, storageFile.Name, 
+                            NameCollisionOption.ReplaceExisting);
+                        uriPath = newFile.Path;
+                        PreviewImage.Source = new BitmapImage(new Uri(uriPath));
+                    }
+                    else
+                    {
+                        
+                        return;
+                    }
+                }
+
+            }
+        }
+
+        private void DropPanel_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+
+            //e.DragUIOverride.Caption = "Your image here";
+            e.DragUIOverride.IsCaptionVisible = true;
+            e.DragUIOverride.IsContentVisible = true;
+            //e.DragUIOverride.IsGlyphVisible = true;
+        }
+
+        private void SplitViewButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void SerializeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
